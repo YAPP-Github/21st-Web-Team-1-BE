c1770d35425936bf29ed4d2d01e50d187a2bc438
jinsim <simsime@korea.ac.kr>
2022-11-30 19:38:48 +0900

    feat : 스프링 프로젝트 생성 (#2)

---
+++ spring/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+	java
+	id("org.springframework.boot") version "2.7.6"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+}
+
+group = "com.yapp"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+
+	// DataBase 의존성
+	runtimeOnly("com.h2database:h2")
+	runtimeOnly("com.mysql:mysql-connector-j")
+
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ spring/src/test/java/com/yapp/memeserver/MemeserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yapp.memeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
