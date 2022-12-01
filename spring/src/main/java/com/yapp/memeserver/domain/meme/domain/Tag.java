a583bb1a04092325ea1ca9139a195fc5646d0ee9
jinsim <simsime@korea.ac.kr>
2022-12-01 14:58:09 +0900

    feat : Tag 엔티티 작성 (#4) - MemeTag 다대다 테이블 생성

---
+++ spring/src/main/java/com/yapp/memeserver/domain/meme/domain/Tag.java
@@ -0,0 +1,23 @@
+package com.yapp.memeserver.domain.meme.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "tag_id")
+    private Long id;
+
+    @NotNull(message = "이름은 필수로 입력되어야 합니다.")
+    @Column(length = 50)
+    private String name;
+}
