edf21504f9f409c4613c4eb81cb055ea3eb6be71
jinsim <simsime@korea.ac.kr>
2023-01-03 21:21:07 +0900

    [YW-126] fix: Meme 변경 테스트 반영 (#34)

--- spring/src/test/java/com/yapp/memeserver/domain/meme/domain/MemeTagTest.java
+++ spring/src/test/java/com/yapp/memeserver/domain/meme/domain/MemeTagTest.java
@@ -7,12 +7,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MemeTagTest {
     @Test
     void 밈_생성_테스트() {
-        String title = "밈 제목";
+        String name = "밈 제목";
         String imageUrl = "https://user-images.githubusercontent.com/62461857/201794604-7f9f6389-1bc9-44e2-8dbb-eb7b1fd3c513.png";
-        String name = "태그명";
+        String tagName = "태그명";
 
-        Meme meme = createMeme(title, imageUrl);
-        Tag tag = createTag(name);
+        Meme meme = createMeme(name, imageUrl);
+        Tag tag = createTag(tagName);
         MemeTag memeTag = createMemeTag(meme, tag);
 
         assertThat(memeTag.getMeme()).isEqualTo(meme);
@@ -26,9 +26,9 @@ class MemeTagTest {
                 .build();
     }
 
-    private Meme createMeme(String title, String imageUrl) {
+    private Meme createMeme(String name, String imageUrl) {
         return Meme.builder()
-                .title(title)
+                .name(name)
                 .imageUrl(imageUrl)
                 .build();
     }
--- spring/src/test/java/com/yapp/memeserver/domain/meme/domain/MemeTest.java
+++ spring/src/test/java/com/yapp/memeserver/domain/meme/domain/MemeTest.java
@@ -8,33 +8,36 @@ class MemeTest {
 
     @Test
     void 밈_생성_테스트() {
-        String title = "밈 제목";
+        String name = "밈 이름";
+        String description = "밈 설명입니다.";
         String imageUrl = "https://user-images.githubusercontent.com/62461857/201794604-7f9f6389-1bc9-44e2-8dbb-eb7b1fd3c513.png";
 
-        Meme meme = createMeme(title, imageUrl);
+        Meme meme = createMeme(name, description, imageUrl);
 
-        assertThat(meme.getTitle()).isEqualTo(title);
+        assertThat(meme.getName()).isEqualTo(name);
         assertThat(meme.getImageUrl()).isEqualTo(imageUrl);
         assertThat(meme.getViewCount()).isEqualTo(0);
     }
 
     @Test
     void 밈_수정_테스트() {
-        String title = "밈 제목";
+        String name = "밈 이름";
         String imageUrl = "https://user-images.githubusercontent.com/62461857/201794604-7f9f6389-1bc9-44e2-8dbb-eb7b1fd3c513.png";
-        String newTitle = "새 밈 제목";
+        String newName = "새 밈 이름";
+        String description = "새 밈 설명입니다.";
         String newImageUrl = "https://user-images.githubusercontent.com/62461857/201794714-a8e7ed02-f327-4c8b-840f-afce02805759.png";
 
-        Meme meme = createMeme(title, imageUrl);
-        meme.updateMeme(newTitle, newImageUrl);
+        Meme meme = createMeme(name, description, imageUrl);
+        meme.updateMeme(newName, description, newImageUrl);
 
-        assertThat(meme.getTitle()).isEqualTo(newTitle);
+        assertThat(meme.getName()).isEqualTo(newName);
         assertThat(meme.getImageUrl()).isEqualTo(newImageUrl);
     }
 
-    private Meme createMeme(String title, String imageUrl) {
+    private Meme createMeme(String name, String description, String imageUrl) {
         return Meme.builder()
-                .title(title)
+                .name(name)
+                .description(description)
                 .imageUrl(imageUrl)
                 .build();
     }
