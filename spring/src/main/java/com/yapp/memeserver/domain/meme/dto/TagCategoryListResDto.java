12c2378c8e7049beb97e59d346a7381e2a159025
jinsim <simsime@korea.ac.kr>
2023-01-05 20:55:48 +0900

    [YW-129] feat: 카테고리 태그 Dto 추가 (#39)

---
+++ spring/src/main/java/com/yapp/memeserver/domain/meme/dto/TagCategoryListResDto.java
@@ -0,0 +1,43 @@
+package com.yapp.memeserver.domain.meme.dto;
+
+import com.yapp.memeserver.domain.meme.domain.Category;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class TagCategoryListResDto {
+
+    private List<SingleCategory> categories;
+
+    @Getter
+    public static class SingleCategory {
+        private Long categoryId;
+        private String name;
+        private Integer priority;
+        private List<TagListResDto.SingleTag> tags;
+
+        public SingleCategory(Category category, List<TagListResDto.SingleTag> tagList) {
+            this.name = category.getName();
+            this.categoryId = category.getId();
+            this.priority = category.getPriority();
+            this.tags = tagList;
+        }
+
+        public static SingleCategory of(Category category) {
+            return new SingleCategory(category, category.getTagList().stream().map(TagListResDto.SingleTag::of).collect(Collectors.toList()));
+        }
+    }
+
+    public static TagCategoryListResDto of(List<Category> categoryList) {
+        return TagCategoryListResDto.builder()
+                .categories(categoryList.stream().map(SingleCategory::of).collect(Collectors.toList()))
+                .build();
+    }
+}
