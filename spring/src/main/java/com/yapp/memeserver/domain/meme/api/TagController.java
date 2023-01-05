a0d54f118380ad60e548e5219cec3ce1953524d5
jinsim <simsime@korea.ac.kr>
2023-01-05 23:47:28 +0900

    [YW-129] feat: 맘별 태그 조회 API 작성 (#39)

--- spring/src/main/java/com/yapp/memeserver/domain/meme/api/TagController.java
+++ spring/src/main/java/com/yapp/memeserver/domain/meme/api/TagController.java
@@ -2,9 +2,12 @@ package com.yapp.memeserver.domain.meme.api;
 
 import com.yapp.memeserver.domain.meme.domain.Category;
 import com.yapp.memeserver.domain.meme.domain.Meme;
+import com.yapp.memeserver.domain.meme.domain.MemeTag;
 import com.yapp.memeserver.domain.meme.domain.Tag;
 import com.yapp.memeserver.domain.meme.dto.*;
 import com.yapp.memeserver.domain.meme.service.CategoryService;
+import com.yapp.memeserver.domain.meme.service.MemeService;
+import com.yapp.memeserver.domain.meme.service.MemeTagService;
 import com.yapp.memeserver.domain.meme.service.TagService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +26,8 @@ public class TagController {
 
     private final TagService tagService;
     private final CategoryService categoryService;
+    private final MemeService memeService;
+    private final MemeTagService memeTagService;
 
     @GetMapping()
     @ResponseStatus(value = HttpStatus.OK)
@@ -51,9 +56,19 @@ public class TagController {
 
     @GetMapping("/categories")
     @ResponseStatus(value = HttpStatus.OK)
-    public TagCategoryListResDto readTagCetegory(   ) {
+    public TagCategoryListResDto getTagCetegory() {
         List<Category> categoryList = categoryService.findAllOrderByPriority();
         TagCategoryListResDto resDto = TagCategoryListResDto.of(categoryList);
+        return resDto;
+    }
+
+    @GetMapping("/memes/{memeId}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public TagListResDto getTagMeme(@PathVariable final Long memeId) {
+        Meme meme = memeService.findById(memeId);
+        List<MemeTag> memeTagList = memeTagService.findByMeme(meme);
+        List<Tag> tagList = memeTagService.findTagMemeList(memeTagList);
+        TagListResDto resDto = TagListResDto.of(tagList);
         return resDto;
     }
 }
