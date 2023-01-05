46505043f03eb07ab1e15869784a09bcd161857a
jinsim <simsime@korea.ac.kr>
2023-01-05 20:57:39 +0900

    [YW-129] feat: 카테고리별 태그 API 작성 (#39) - Controller, Repository, Service 작성

--- spring/src/main/java/com/yapp/memeserver/domain/meme/repository/CategoryRepository.java
+++ spring/src/main/java/com/yapp/memeserver/domain/meme/repository/CategoryRepository.java
@@ -2,7 +2,11 @@ package com.yapp.memeserver.domain.meme.repository;
 
 import com.yapp.memeserver.domain.meme.domain.Category;
 import com.yapp.memeserver.domain.meme.domain.MemeTag;
+import com.yapp.memeserver.domain.meme.domain.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findAllByOrderByPriorityAsc();
 }
