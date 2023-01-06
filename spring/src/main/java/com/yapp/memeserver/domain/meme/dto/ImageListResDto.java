f9ce755a10b579ad6c3ad671f34b88a2a31af07f
jinsim <simsime@korea.ac.kr>
2023-01-06 14:29:53 +0900

    [YW-130] feat: Image Service, Repository, Dto 작성(#41)

---
+++ spring/src/main/java/com/yapp/memeserver/domain/meme/dto/ImageListResDto.java
@@ -0,0 +1,48 @@
+package com.yapp.memeserver.domain.meme.dto;
+
+import com.yapp.memeserver.domain.meme.domain.Category;
+import com.yapp.memeserver.domain.meme.domain.Image;
+import com.yapp.memeserver.domain.meme.domain.Meme;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ImageListResDto {
+
+    private List<SingleImage> images;
+    private Integer count;
+
+    @Getter
+    public static class SingleImage {
+        private Long imageId;
+        private String imageUrl;
+        private Integer width;
+        private Integer height;
+
+        public SingleImage(Image image) {
+            this.imageId = image.getId();
+            this.imageUrl = image.getImageUrl();
+            this.width = image.getWidth();
+            this.height = image.getHeight();
+        }
+        
+        public static SingleImage of(Image image) {
+            return new SingleImage(image);
+        }
+    }
+    public static ImageListResDto of(List<Image> imageList) {
+        return ImageListResDto.builder()
+                .images(imageList.stream().map(SingleImage::of).collect(Collectors.toList()))
+                .count(imageList.size())
+                .build();
+    }
+
+}
---
+++ spring/src/main/java/com/yapp/memeserver/domain/meme/repository/ImageRepository.java
@@ -0,0 +1,13 @@
+package com.yapp.memeserver.domain.meme.repository;
+
+import com.yapp.memeserver.domain.meme.domain.Image;
+import com.yapp.memeserver.domain.meme.domain.Meme;
+import com.yapp.memeserver.domain.meme.domain.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+    List<Image> findByMeme(Meme meme);
+}
