a5ee1aed1cd9a25ffb339880868479fb98eef485
jinsim <simsime@korea.ac.kr>
2023-01-06 14:41:25 +0900

    [YW-130] feat: 태그별 밈 조회 API 작성 (#41)

--- spring/src/main/java/com/yapp/memeserver/domain/meme/repository/MemeTagRepository.java
+++ spring/src/main/java/com/yapp/memeserver/domain/meme/repository/MemeTagRepository.java
@@ -2,6 +2,7 @@ package com.yapp.memeserver.domain.meme.repository;
 
 import com.yapp.memeserver.domain.meme.domain.Meme;
 import com.yapp.memeserver.domain.meme.domain.MemeTag;
+import com.yapp.memeserver.domain.meme.domain.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +10,5 @@ import java.util.List;
 public interface MemeTagRepository extends JpaRepository<MemeTag, Long> {
 
     List<MemeTag> findByMeme(Meme meme);
+    List<MemeTag> findByTag(Tag tag);
 }
