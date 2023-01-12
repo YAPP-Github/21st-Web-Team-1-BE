36ac05bec8006f720251ec7c54fca46bd4577f7f
jinsim <simsime@korea.ac.kr>
2023-01-13 01:25:03 +0900

    [YW-138] fix: S3에 저장된 이미지로 데이터 변경 (#53)

--- spring/src/main/resources/sql/data.sql
+++ spring/src/main/resources/sql/data.sql
@@ -4,13 +4,13 @@ INSERT INTO meme(name, description, view_count, share_count, created_date) VALUE
 INSERT INTO meme(name, description, view_count, share_count, created_date) VALUES('평생 입 다물게 해줄까?', '박명수가 무한도전 123화에서 한 말', 445, 5, now());
 INSERT INTO meme(name, description, view_count, share_count, created_date) VALUES('그래도... 일은 해야지...', '무한도전 중 박명수 어록 중 하나', 455, 5, now());
 
-INSERT INTO image(image_url, width, height, meme_id) VALUES('https://user-images.githubusercontent.com/62461857/210932543-7a8c2025-f162-4ffa-8486-8ca425cc2cf8.png', 960, 987, 1);
-INSERT INTO image(image_url, width, height, meme_id) VALUES('https://user-images.githubusercontent.com/62461857/210932649-cc578130-3689-4b78-941b-d42828127b79.png', 517, 706, 2);
-INSERT INTO image(image_url, width, height, meme_id) VALUES('https://user-images.githubusercontent.com/62461857/210932697-6609ff9b-431c-4eab-ac77-060ce29a9222.png', 466, 631, 2);
-INSERT INTO image(image_url, width, height, meme_id) VALUES('https://user-images.githubusercontent.com/62461857/210932754-083d9814-691e-4ce0-8e9f-8f1fe7eb6f2e.png', 488, 566, 2);
-INSERT INTO image(image_url, width, height, meme_id) VALUES('https://user-images.githubusercontent.com/62461857/210932924-a7c42ce4-266d-4cbd-ae0d-8f09dd4f8a50.png', 225, 225, 3);
-INSERT INTO image(image_url, width, height, meme_id) VALUES('https://user-images.githubusercontent.com/62461857/211177350-892daee2-3438-4fd9-a14d-6d10d3f69c74.png', 686, 377, 4);
-INSERT INTO image(image_url, width, height, meme_id) VALUES('https://user-images.githubusercontent.com/62461857/211178127-989bde34-a0cd-4dd9-81b7-abbf44b7deef.png', 609, 609, 5);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/1.png', 960, 987, 1);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/2.png', 517, 706, 2);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/3.png', 466, 631, 2);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/4.png', 488, 566, 2);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/5.png', 225, 225, 3);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/6.png', 686, 377, 4);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/7.png', 609, 609, 5);
 
 INSERT INTO category(name, priority) VALUES('직업', 100);
 INSERT INTO category(name, priority) VALUES('캐릭터', 200);
