30d0b61c313796a29d4d06cc727096bda81e0502
jinsim <simsime@korea.ac.kr>
2023-01-19 17:47:55 +0900

    [YW-131] chore: 무한도전 밈 추가 (#43)

--- spring/src/main/resources/sql/data.sql
+++ spring/src/main/resources/sql/data.sql
@@ -3,6 +3,19 @@ INSERT INTO meme(name, description, view_count, share_count, created_date) VALUE
 INSERT INTO meme(name, description, view_count, share_count, created_date) VALUES('페페', '초록 개구리 페페 밈입니다.', 45, 5, now());
 INSERT INTO meme(name, description, view_count, share_count, created_date) VALUES('평생 입 다물게 해줄까?', '박명수가 무한도전 123화에서 한 말', 445, 5, now());
 INSERT INTO meme(name, description, view_count, share_count, created_date) VALUES('그래도... 일은 해야지...', '무한도전 중 박명수 어록 중 하나', 455, 5, now());
+INSERT INTO meme(name, description, view_count, share_count, created_date) VALUES('아~', '무한도전 박명수 하품', 101, 5, now());
+INSERT INTO meme(name, description, view_count, share_count, created_date) VALUES('종잡을 수 없는 흐름에 다들 소름', '무한도전 양세형 소름', 102, 5, now());
+INSERT INTO meme(name, description, view_count, share_count, created_date) VALUES('요만큼도 없다', '무한도전 유재석 밈', 103, 5, now());
+INSERT INTO meme(name, description, view_count, share_count, created_date) VALUES('속이 속이 아니잖아요...', '무한도전 유재석 밈', 104, 5, now());
+INSERT INTO meme(name, description, view_count, share_count, created_date) VALUES('사이버머니 조심해요', '무한도전 유재석 밈', 105, 5, now());
+INSERT INTO meme(name, description, view_count, share_count, created_date) VALUES('재수 없으니까 가만히 좀', '무한도전 박명수 밈', 106, 5, now());
+INSERT INTO meme(name, description, view_count, share_count, created_date) VALUES('입 좀 다물고 기다리시죠?', '무한도전 유재석 밈', 107, 5, now());
+INSERT INTO meme(name, description, view_count, share_count, created_date) VALUES('조용히 하지 못할까 이 요망한 것!', '무한도전 유재석 밈', 108, 5, now());
+INSERT INTO meme(name, description, view_count, share_count, created_date) VALUES('좀 이해해주십시오! 화내서 미안합니다', '무한도전 유재석 밈', 109, 5, now());
+INSERT INTO meme(name, description, view_count, share_count, created_date) VALUES('사기꾼이에요?', '무한도전 유재석 밈', 110, 5, now());
+INSERT INTO meme(name, description, view_count, share_count, created_date) VALUES('세월을 우리만 맞은 듯', '무한도전 유재석 밈', 110, 5, now());
+INSERT INTO meme(name, description, view_count, share_count, created_date) VALUES('두 개를 가질 수는 없겠더라고...', '무한도전 유재석 밈', 111, 5, now());
+INSERT INTO meme(name, description, view_count, share_count, created_date) VALUES('일이 많아!', '무한도전 유재석 밈', 112, 5, now());
 
 INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/1.png', 960, 987, 1);
 INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/2.png', 517, 706, 2);
@@ -11,6 +24,19 @@ INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebuc
 INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/5.png', 225, 225, 3);
 INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/6.png', 686, 377, 4);
 INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/7.png', 609, 609, 5);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/8.png', 591, 746, 6);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/9.png', 1200, 675, 7);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/10.png', 1920, 1080, 8);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/11.png', 1280, 720, 9);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/12.png', 330, 153, 10);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/13.png', 320, 287, 11);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/14.png', 600, 457, 12);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/15.png', 400, 403, 13);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/16.png', 800, 450, 14);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/17.png', 600, 471, 15);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/18.png', 1200, 1200, 16);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/19.png', 600, 659, 17);
+INSERT INTO image(image_url, width, height, meme_id) VALUES('https://yappmemebucket.s3.ap-northeast-2.amazonaws.com/memes/images/20.png', 600, 476, 18);
 
 INSERT INTO category(name, priority) VALUES('직업', 100);
 INSERT INTO category(name, priority) VALUES('캐릭터', 200);
