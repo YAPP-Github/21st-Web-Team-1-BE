816f12ec58ba961bd1e47b1cd5504b191e047257
jinsim <simsime@korea.ac.kr>
2023-02-04 11:54:10 +0900

    [YW-195] fix: blacklist에 remove refresh token 추가 (#62)

--- spring/src/main/java/com/yapp/memeserver/domain/auth/api/AuthController.java
+++ spring/src/main/java/com/yapp/memeserver/domain/auth/api/AuthController.java
@@ -5,10 +5,14 @@ import com.yapp.memeserver.domain.auth.dto.AccessTokenDto;
 import com.yapp.memeserver.domain.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
+import java.util.concurrent.TimeUnit;
+
+import static org.springframework.http.HttpHeaders.SET_COOKIE;
 
 @Slf4j
 @RestController
@@ -32,8 +36,19 @@ public class AuthController {
     @PostMapping("/blacklist")
     public ResponseEntity<AccessTokenDto> signOut(@RequestBody AccessTokenDto requestDto) {
         AccessTokenDto resDto = authService.signOut(requestDto);
+        ResponseCookie responseCookie = removeRefreshTokenCookie();
 
         return ResponseEntity.ok()
+                .header(SET_COOKIE, responseCookie.toString())
                 .body(resDto);
+    }
+
+    public ResponseCookie removeRefreshTokenCookie() {
+        return ResponseCookie.from("refreshToken1", null)
+                .path("/") // 해당 경로 하위의 페이지에서만 쿠키 접근 허용. 모든 경로에서 접근 허용한다.
+                .maxAge(0) // 쿠키의 expiration 타임을 0으로 하여 없앤다.
+                .secure(true) // HTTPS로 통신할 때만 쿠키가 전송된다.
+                .httpOnly(true) // JS를 통한 쿠키 접근을 막아, XSS 공격 등을 방어하기 위한 옵션이다.
+                .build();
     }
 }
