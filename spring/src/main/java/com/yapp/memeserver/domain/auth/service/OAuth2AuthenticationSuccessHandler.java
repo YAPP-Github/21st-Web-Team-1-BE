8b33503a2a099ab01a8cc55daf54d70aa293f0a3
jinsim <simsime@korea.ac.kr>
2023-02-04 11:53:50 +0900

    [YW-195] fix: cookie에 samesite none 추가 (#62)

--- spring/src/main/java/com/yapp/memeserver/domain/auth/service/OAuth2AuthenticationSuccessHandler.java
+++ spring/src/main/java/com/yapp/memeserver/domain/auth/service/OAuth2AuthenticationSuccessHandler.java
@@ -65,6 +65,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
                 .path("/") // 해당 경로 하위의 페이지에서만 쿠키 접근 허용. 모든 경로에서 접근 허용한다.
                 .maxAge(TimeUnit.MILLISECONDS.toSeconds(refreshTokenValidationMs)) // 쿠키 만료 시기(초). 없으면 브라우저 닫힐 때 제거
                 .secure(true) // HTTPS로 통신할 때만 쿠키가 전송된다.
+                .sameSite("none")
                 .httpOnly(true) // JS를 통한 쿠키 접근을 막아, XSS 공격 등을 방어하기 위한 옵션이다.
                 .build();
     }
