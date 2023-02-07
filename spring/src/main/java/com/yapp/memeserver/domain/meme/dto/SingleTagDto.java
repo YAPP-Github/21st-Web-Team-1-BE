2a940a581eee85fbf4a06917813256ad1eae8818
jinsim <simsime@korea.ac.kr>
2023-02-07 20:51:26 +0900

    [YW-172] feat: Tag DTO에 isFav 와 setFav 추가 (#67)

--- spring/src/main/java/com/yapp/memeserver/domain/meme/dto/SingleTagDto.java
+++ spring/src/main/java/com/yapp/memeserver/domain/meme/dto/SingleTagDto.java
@@ -14,14 +14,20 @@ public class SingleTagDto {
     private Long tagId;
     private String name;
     private Integer viewCount;
+    private Boolean isFav;
 
     public SingleTagDto(Tag tag) {
         this.tagId = tag.getId();
         this.name = tag.getName();
         this.viewCount = tag.getViewCount();
+        this.isFav = false;
     }
 
     public static SingleTagDto of(Tag tag) {
         return new SingleTagDto(tag);
+    }
+
+    public void setFav(boolean isFav) {
+        this.isFav = isFav;
     }
 }
