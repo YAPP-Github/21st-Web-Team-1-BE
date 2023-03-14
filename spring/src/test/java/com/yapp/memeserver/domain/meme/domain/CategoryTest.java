f82071d2ac62bbd65362e3df0f288f69fd3269a0
jinsim <simsime@korea.ac.kr>
2023-03-14 22:27:15 +0900

    feat : categoryTest 작성 (#96)

---
+++ spring/src/test/java/com/yapp/memeserver/domain/meme/domain/CategoryTest.java
@@ -0,0 +1,42 @@
+package com.yapp.memeserver.domain.meme.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CategoryTest {
+
+    @Test
+    void 카테고리_생성_테스트() {
+        String name = "카테고리명";
+        String icon = "https://raw.githubusercontent.com/toss/tossface/cec7ea0420b7f17d6f546fd7359da9bd4cb3315c/dist/svg/u1F60E.svg";
+
+        Category category = createCategory(name, icon);
+
+        assertThat(category.getName()).isEqualTo(name);
+        assertThat(category.getIcon()).isEqualTo(icon);
+    }
+
+    @Test
+    void 카테고리_수정_테스트() {
+        String name = "카테고리명";
+        String icon = "https://raw.githubusercontent.com/toss/tossface/cec7ea0420b7f17d6f546fd7359da9bd4cb3315c/dist/svg/u1F60E.svg";
+
+        String newName = "새 카테고리명";
+        String newIcon = "https://raw.githubusercontent.com/toss/tossface/cec7ea0420b7f17d6f546fd7359da9bd4cb3315c/dist/svg/u1FAF5.svg";
+
+
+        Category category = createCategory(name, icon);
+        category.updateCategory(newName, newIcon);
+
+        assertThat(category.getName()).isEqualTo(newName);
+        assertThat(category.getIcon()).isEqualTo(newIcon);
+    }
+
+    private Category createCategory(String name, String icon) {
+        return Category.builder()
+                .name(name)
+                .icon(icon)
+                .build();
+    }
+}
