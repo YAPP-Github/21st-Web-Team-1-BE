e5cc837a8275513aacda8e33830f89121a1233e6
jinsim <simsime@korea.ac.kr>
2023-04-08 09:52:24 +0900

    feat : 카테고리 메소드 작성 (#104)

--- spring/src/main/java/com/yapp/memeserver/domain/meme/service/CategoryService.java
+++ spring/src/main/java/com/yapp/memeserver/domain/meme/service/CategoryService.java
@@ -1,6 +1,7 @@
 package com.yapp.memeserver.domain.meme.service;
 
 import com.yapp.memeserver.domain.meme.domain.Category;
+import com.yapp.memeserver.domain.meme.domain.MainCategory;
 import com.yapp.memeserver.domain.meme.domain.Tag;
 import com.yapp.memeserver.domain.meme.repository.CategoryRepository;
 import com.yapp.memeserver.domain.meme.repository.TagRepository;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.HashMap;
 import java.util.List;
 
 @Slf4j
@@ -20,13 +22,36 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
+    private final TagRepository tagRepository;
+
     @Transactional(readOnly = true)
     public Category findById(Long categoryId) {
         return categoryRepository.findById(categoryId)
-                .orElseThrow(() -> new EntityNotFoundException("해당 id를 가진 Tag 을 찾을 수 없습니다. id = "+ categoryId ));
+                .orElseThrow(() -> new EntityNotFoundException("해당 id를 가진 Category 을 찾을 수 없습니다. id = "+ categoryId ));
     }
 
     public List<Category> findAllOrderByPriority() {
         return categoryRepository.findAllByOrderByPriorityAsc();
+    }
+
+    @Transactional(readOnly = true)
+    public List<Category> findByMainCategory(MainCategory mainCategory) {
+        return categoryRepository.findByMainCategoryOrderByPriorityAsc(mainCategory);
+    }
+
+    @Transactional(readOnly = true)
+    public HashMap<Long, HashMap<Category, List<Tag>>> getCategoryMap(List<MainCategory> mainCategoryList) {
+        HashMap<Long, HashMap<Category, List<Tag>>> categoryMap = new HashMap<>();
+        for(MainCategory mainCategory : mainCategoryList) {
+            HashMap<Category, List<Tag>> tagMap = new HashMap<>();
+            List<Category> categoryList = findByMainCategory(mainCategory);
+            for (Category category : categoryList) {
+                Long categoryId = category.getId();
+                List<Tag> tagList = tagRepository.findCategoryTag(categoryId);
+                tagMap.put(category, tagList);
+            }
+            categoryMap.put(mainCategory.getId(), tagMap);
+        }
+        return categoryMap;
     }
 }
