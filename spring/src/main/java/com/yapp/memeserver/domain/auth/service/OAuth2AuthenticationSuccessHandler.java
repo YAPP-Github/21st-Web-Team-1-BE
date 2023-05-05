a019e3130480cb0e10d35828f79a74365612b112
jinsim <simsime@korea.ac.kr>
2023-05-05 09:37:51 +0900

    feat : Cookie를 통한 nextPageUrl 전달 (#68)

--- spring/src/main/java/com/yapp/memeserver/domain/auth/service/OAuth2AuthenticationSuccessHandler.java
+++ spring/src/main/java/com/yapp/memeserver/domain/auth/service/OAuth2AuthenticationSuccessHandler.java
@@ -14,9 +14,11 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationSu
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
@@ -31,12 +33,13 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException {
-        String referer = request.getHeader("Referer");
 
         DefaultOAuth2User principal = (DefaultOAuth2User) authentication.getPrincipal();
         Map<String, Object> attributes = principal.getAttributes();
         String email = (String) ((Map<String, Object>) principal.getAttributes().get("kakao_account")).get("email");
-        String url = makeRedirectUrl(email, (String) attributes.get("subDomain"));
+        String nextPageUrl = getNextPageUrl(request);
+
+        String url = makeRedirectUrl(email, (String) attributes.get("subDomain"), nextPageUrl);
         ResponseCookie responseCookie = generateRefreshTokenCookie(email);
         response.setHeader("Set-Cookie", responseCookie.toString());
         response.getWriter().write(url);
@@ -49,16 +52,34 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         getRedirectStrategy().sendRedirect(request, response, url);
     }
 
+    private String getNextPageUrl(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        String nextPageUrl = null;
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("nextPageUrl")) {
+                    nextPageUrl = cookie.getValue();
+                    break;
+                }
+            }
+        }
+        return nextPageUrl;
+    }
+
     // UTF-8로 인코딩 해서 반환.
-    private String makeRedirectUrl(String email, String subDomain) {
+    private String makeRedirectUrl(String email, String subDomain, String nextPageUrl) {
         if (Objects.equals(subDomain, "kakao")) {
             subDomain = "app";
+        }
+        if (nextPageUrl == null) {
+            nextPageUrl = "https://" + subDomain + ".thismeme.me/";
         }
 
         String accessToken = jwtProvider.generateAccessToken(email);
 
-        return UriComponentsBuilder.fromHttpUrl("https://" + subDomain + ".thismeme.me/" +"oauth2/redirect")
+        return UriComponentsBuilder.fromHttpUrl("https://" + subDomain + ".thismeme.me/" + "oauth2/redirect")
                 .queryParam("accessToken", accessToken)
+                .queryParam("nextPageUrl", nextPageUrl)
                 .build()
                 .encode()
                 .toUriString();
